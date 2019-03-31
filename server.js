6c4234617bd964e96738d818ef20191212800ee7
Andrew Samole <samole@live.com>
2019-03-30 22:45:25 -0400

    Express Route

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
