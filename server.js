ee42d722f4e6760fa9025e039e2beb1dcd9584a5
Andrew Samole <samole@live.com>
2019-03-31 00:04:09 -0400

    Downgraded mongodb

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 var express = require("express");
+var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
-var path = require("path");
+var MongoClient = require('mongodb').MongoClient;
 var ObjectID = mongodb.ObjectID;
 
-var ITEMS_COLLECTION = "items";
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-
-// Create link to Angular build directory
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
+const uri = "mongodb://heroku_c7hgq62n:fojo3i23k1h5l5n78fbm82f8tu@ds137826.mlab.com:37826/heroku_c7hgq62n";
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+// Connect to the database before starting the application server. 
+MongoClient.connect(uri, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
   // Save database object from the callback for reuse.
-  db = client.db();
+  db = database;
   console.log("Database connection ready");
 
   // Initialize the app.
@@ -35,7 +33,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   });
 });
 
-// ITEMS API ROUTES BELOW
+// CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -43,74 +41,73 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/api/items"
- *    GET: finds all items
- *    POST: creates a new item
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
  */
 
-app.get("/api/items", function(req, res) {
-  db.collection(ITEMS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get items.");
+      handleError(res, err.message, "Failed to get contacts.");
     } else {
-      res.status(200).json(docs);
+      res.status(200).json(docs);  
     }
   });
 });
 
-app.post("/api/items", function(req, res) {
+app.post("/contacts", function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
-  if (!req.body.name) {
-    handleError(res, "Invalid user input", "Must provide a name.", 400);
-  } else {
-    db.collection(ITEMS_COLLECTION).insertOne(newContact, function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to create new item.");
-      } else {
-        res.status(201).json(doc.ops[0]);
-      }
-    });
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
   }
-});
 
-/*  "/api/items/:id"
- *    GET: find item by id
- *    PUT: update item by id
- *    DELETE: deletes item by id
- */
-
-app.get("/api/items/:id", function(req, res) {
-  db.collection(ITEMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get item");
+      handleError(res, err.message, "Failed to create new contact.");
     } else {
-      res.status(200).json(doc);
+      res.status(201).json(doc.ops[0]);
     }
   });
 });
 
-app.put("/api/items/:id", function(req, res) {
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);  
+    }
+  });
+});
+
+app.put("/contacts/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(ITEMS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to update item");
+      handleError(res, err.message, "Failed to update contact");
     } else {
-      updateDoc._id = req.params.id;
-      res.status(200).json(updateDoc);
+      res.status(204).end();
     }
   });
 });
 
-app.delete("/api/items/:id", function(req, res) {
-  db.collection(ITEMS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete item");
+      handleError(res, err.message, "Failed to delete contact");
     } else {
-      res.status(200).json(req.params.id);
+      res.status(204).end();
     }
   });
 });
