265b680a19fd1bf98ae098a30d23935d37d9b0b5
Andrew Samole <samole@live.com>
2019-04-01 14:16:38 -0400

    Added Second Dropdown

--- server.js
+++ server.js
@@ -1,113 +1,84 @@
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = mongodb.ObjectID;
+// var mongodb = require("mongodb");
+// var MongoClient = require('mongodb').MongoClient;
+// var ObjectID = mongodb.ObjectID;
+// var http = require('http');
 
-var CONTACTS_COLLECTION = "contacts";
+// var SPEAKERS_COLLECTION = "speakers";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-const uri = "mongodb://heroku_c7hgq62n:fojo3i23k1h5l5n78fbm82f8tu@ds137826.mlab.com:37826/heroku_c7hgq62n";
-
-// Connect to the database before starting the application server. 
-MongoClient.connect(uri, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-});
 
-// CONTACTS API ROUTES BELOW
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+// var db;
 
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
+// Connect to the database before starting the application server. 
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
+// http.get('https://api.itorah.com/api/Speakers/allspeakers').then(function(err, response) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+//   db = response.data;
 
-app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
-    } else {
-      res.status(200).json(docs);  
-    }
-  });
-});
+//   // Save database object from the callback for reuse.
+//   console.log("Database connection ready");
 
-app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
-  }
+// // SPEAKERS API ROUTES BELOW
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
+// // Generic error handler used by all endpoints.
+// function handleError(res, reason, message, code) {
+//   console.log("ERROR: " + reason);
+//   res.status(code || 500).json({"error": message});
+// }
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
+//   "/speakers"
+//  *    GET: finds all speakers
+//  *    POST: creates a new contact
+ 
 
-app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contact");
-    } else {
-      res.status(200).json(doc);  
-    }
-  });
-});
+// app.get("/speakers", function(req, res) {
+//   db.collection(SPEAKERS_COLLECTION).find({}).toArray(function(err, docs) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get speakers.");
+//     } else {
+//       res.status(200).json(docs);  
+//     }
+//   });
+// });
 
-app.put("/contacts/:id", function(req, res) {
-  var updateDoc = req.body;
-  delete updateDoc._id;
 
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
 
-app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
+// /*  "/speakers/:id"
+//  *    GET: find contact by id
+//  *    PUT: update contact by id
+//  *    DELETE: deletes contact by id
+//  */
+
+// app.get("/speakers/:id", function(req, res) {
+//   db.collection(SPEAKERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get contact");
+//     } else {
+//       res.status(200).json(doc);  
+//     }
+//   });
+// });
+
+
+
