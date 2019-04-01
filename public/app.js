fad72efb6d144628b1a572e37a18fe3a428a3b20
Andrew Samole <samole@live.com>
2019-04-01 15:59:27 -0400

    Finished

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
 var app = angular.module("3nom", []);
 
 app.controller("myCtrl", function($scope, $http) {
+  $scope.selectedSpeaker = "";
+  $scope.selectedCat = "";
+  $scope.selectedSubCat = "";
+
   $http.get('https://api.itorah.com/api/Speakers/allspeakers').
   then(function(response) {
     var data = response.data;
@@ -8,11 +12,7 @@ app.controller("myCtrl", function($scope, $http) {
     for (var i = 0; i <= data.length; i++) {
       if(typeof data[i] !== 'undefined'){
        if (data[i].isMainSpeaker === true){
-
         data[i].speaker =  "Main Speaker: "+ data[i].speaker;
-
-
-        
       }
       else{
         data[i].speaker = data[i].speaker;
@@ -21,20 +21,53 @@ app.controller("myCtrl", function($scope, $http) {
   }
 
   $scope.speakers = data
-  
+
 });
-  $scope.selectedSpeaker = "";
-var catElement = document.getElementById("category");
-catElement.value = "Please Select a Speaker";
-catElement.disabled = true;
+
+  var catElement = document.getElementById("category");
+  catElement.disabled = true;
+  var scatElement = document.getElementById("scategory");
+  scatElement.disabled = true;
+
   $scope.getCat = function(){
-    
+
     catElement.disabled = false;
-   $http.get("https://api.itorah.com/api/Categories/catfilter?"+$scope.selectedSpeaker.id).
-   then(function(response) {
-    $scope.cats = response.data;
-    $scope.selectedCat = "";
+    $http.get("https://api.itorah.com/api/Categories/catfilter?="+$scope.selectedSpeaker.id).
+    then(function(response) {
+      var categories = response.data;
+      for (var i = 0; i <= categories.length; i++) {
+       if(typeof categories[i] !== 'undefined')
+        categories[i].name = categories[i].name + " (" + categories[i].shiurCount + ")";
+    }
+    $scope.cats = categories;
+
+
+  });
+  }
+
+  $scope.getSubCat = function(){
+    scatElement.disabled = false;
+    $http.get("https://api.itorah.com/api/Categories/subfilter?CategoryID="+$scope.selectedCat.id+"&SpeakerID="+$scope.selectedSpeaker.id).
+    then(function(response) {
+      var categories = response.data;
+      for (var i = 0; i <= categories.length; i++) {
+       if(typeof categories[i] !== 'undefined')
+        categories[i].name = categories[i].name + " (" + categories[i].shiurCount + ")";
+    }
+    $scope.scats = categories;
+
+  });
+  }
+  $scope.getResults = function(){
+    if ($scope.selectedSubCat.id != "")
+      $http.get("https://api.itorah.com/api/Shiurim/all?PageIndex=1&PageSize=20&CategoryID="+$scope.selectedSubCat.id+"&SpeakerID="+$scope.selectedSpeaker.id).
+    then(function(response) {
+      $scope.results =  response.data.shiurList;
+  });
+       else
+      $http.get("https://api.itorah.com/api/Shiurim/all?PageIndex=1&PageSize=20&CategoryID="+$scope.selectedCat.id+"&SpeakerID="+$scope.selectedSpeaker.id).
+    then(function(response) {
+      $scope.results =  response.data.shiurList;
   });
  }
-
 });
